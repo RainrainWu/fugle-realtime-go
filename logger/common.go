2a3a679f67211493de8753c9dae76853a0ce2f75
RainrainWu <s0958334772@gmail.com>
2021-04-20 17:04:19 +0800

    fix(logger): error check for defer logger sync function

--- logger/common.go
+++ logger/common.go
@@ -1,13 +1,22 @@
 package logger
 
-import "go.uber.org/zap"
+import (
+	"go.uber.org/zap"
+)
 
 var (
 	PrintLogger *zap.SugaredLogger
 )
 
+func syncLogger(logger *zap.SugaredLogger) {
+	err := logger.Sync()
+	if err != nil {
+		PrintLogger.Error(err.Error())
+	}
+}
+
 func init() {
 	printLoggerProd, _ := zap.NewProduction()
 	PrintLogger = printLoggerProd.Sugar()
-	defer PrintLogger.Sync()
+	defer syncLogger(PrintLogger)
 }
